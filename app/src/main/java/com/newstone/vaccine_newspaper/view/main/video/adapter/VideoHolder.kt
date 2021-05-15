d9edadb97a58be5e83f687e6177a221973fe7799
hotstone1993 <hotstone1993@gmail.com>
2021-05-16 02:08:36 +0900

    비디오 홀더에 UI 연결

--- app/src/main/java/com/newstone/vaccine_newspaper/view/main/video/adapter/VideoHolder.kt
+++ app/src/main/java/com/newstone/vaccine_newspaper/view/main/video/adapter/VideoHolder.kt
@@ -5,15 +5,29 @@ import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.newstone.vaccine_newspaper.R
 
 class VideoHolder(context: Context, parent: ViewGroup, val videoList: MutableList<VideoItem>) : RecyclerView.ViewHolder(LayoutInflater.from(context).inflate(
-        R.layout.item_news, parent, false)) {
+        R.layout.item_video, parent, false)) {
 
     @RequiresApi(Build.VERSION_CODES.M)
     private fun View.onBind(item: VideoItem) {
+        val preview = itemView.findViewById<ImageView>(R.id.videoPreview)
+        val videoPreviewProgressBar = itemView.findViewById<ProgressBar>(R.id.videoPreviewProgressBar)
+        val videoTimeTextView = itemView.findViewById<TextView>(R.id.videoTimeTextView)
+
+        val titleTextView = itemView.findViewById<TextView>(R.id.videoTitleTextView)
+
+        val channelImage = itemView.findViewById<ImageView>(R.id.channelImage)
+        val channelNameTextView = itemView.findViewById<TextView>(R.id.channelNameTextView)
+        val viewsTextView = itemView.findViewById<TextView>(R.id.viewsTextView)
+        val dateTextView = itemView.findViewById<TextView>(R.id.dateTextView)
+
         itemView.setOnClickListener {
             val pos = adapterPosition
             if(pos != RecyclerView.NO_POSITION) {
