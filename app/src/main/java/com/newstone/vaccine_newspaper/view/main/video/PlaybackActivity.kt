fb6463e5dbb7999de0ac147681015141a9979549
hotstone1993 <hotstone1993@gmail.com>
2021-06-11 02:37:16 +0900

    재생화면에서 전체화면을 기본설정으로 변경

--- app/src/main/java/com/newstone/vaccine_newspaper/view/main/video/PlaybackActivity.kt
+++ app/src/main/java/com/newstone/vaccine_newspaper/view/main/video/PlaybackActivity.kt
@@ -123,6 +123,12 @@ class PlaybackActivity: ComponentActivity(), Player.Listener {
             }
         }
         playerView.setBackgroundColor(resources.getColor(R.color.black, null))
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE or
+        View.SYSTEM_UI_FLAG_FULLSCREEN or
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION)
     }
 
     fun releasePlayer() {
